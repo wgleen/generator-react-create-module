a488b42b00f3fc439713bba8e37a703852874b83
Wallace Gleen <wallace.gleen@gmail.com>
2018-02-11 10:43:10 -0200

    initial commit

---
+++ app/index.js
@@ -0,0 +1,99 @@
+const Generator = require('yeoman-generator')
+const helpers = require('../lib/helpers')
+
+module.exports = class extends Generator {
+  constructor (args, opts) {
+    super(args, opts)
+
+    this.appName = process.argv[3]
+
+    this.log('Initializing react-create-module generator')
+  }
+
+  start () {
+    this.prompt([
+      {
+        type: 'input',
+        name: 'appName',
+        message: `Enter a name for the application ${this.appName ? `or keep ${this.appName}` : ''}`
+      }
+    ])
+    .then(answers => {
+      const _appName = answers.appName || this.appName
+      const appTitle = helpers.toTitle(_appName)
+
+      let destinationPath = `${_appName}/`
+      let templatePath = ''
+
+      // Root files
+
+      this.fs.copy(
+        this.templatePath(`${templatePath}_.gitignore`),
+        this.destinationPath(`${destinationPath}.gitignore`)
+      )
+
+      this.fs.copy(
+        this.templatePath(`${templatePath}.babelrc`),
+        this.destinationPath(`${destinationPath}.babelrc`)
+      )
+
+      this.fs.copyTpl(
+        this.templatePath(`${templatePath}package.json.ejs`),
+        this.destinationPath(`${destinationPath}package.json`),
+        { appName: _appName }
+      )
+
+      this.fs.copy(
+        this.templatePath(`${templatePath}webpack.config.babel.js`),
+        this.destinationPath(`${destinationPath}webpack.config.babel.js`)
+      )
+
+      // Config files
+
+      destinationPath = `${_appName}/config/`
+      templatePath = 'config/'
+
+      this.fs.copy(
+        this.templatePath(`${templatePath}webpackEnvs.js`),
+        this.destinationPath(`${destinationPath}webpackEnvs.js`)
+      )
+
+      this.fs.copy(
+        this.templatePath(`${templatePath}index.js`),
+        this.destinationPath(`${destinationPath}index.js`)
+      )
+
+      // Demo files
+
+      destinationPath = `${_appName}/demo/`
+      templatePath = 'demo/'
+
+      this.fs.copyTpl(
+        this.templatePath(`${templatePath}App.jsx.ejs`),
+        this.destinationPath(`${destinationPath}App.jsx`),
+        { appName: appTitle }
+      )
+
+      this.fs.copyTpl(
+        this.templatePath(`${templatePath}index.html.ejs`),
+        this.destinationPath(`${destinationPath}index.html`),
+        { appName: appTitle }
+      )
+
+      this.fs.copy(
+        this.templatePath(`${templatePath}index.js`),
+        this.destinationPath(`${destinationPath}index.js`)
+      )
+
+      // Src files
+
+      destinationPath = `${_appName}/src/`
+      templatePath = 'src/'
+
+      this.fs.copy(
+        this.templatePath(`${templatePath}index.js`),
+        this.destinationPath(`${destinationPath}index.js`)
+      )
+    })
+  }
+}
---
+++ app/templates/config/index.js
@@ -0,0 +1,8 @@
+import path from 'path'
+const rootPath = path.join(__dirname, '../')
+
+export default {
+  paths: {
+    root: rootPath
+  }
+}
