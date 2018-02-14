333c053a12c8daef6a8a95b49f0b1320f079535b
Wallace Gleen <wallace.gleen@gmail.com>
2018-02-14 13:28:56 -0200

    remove mymodule path

--- mymodule/config/index.js
+++
@@ -1,8 +0,0 @@
-import path from 'path'
-const rootPath = path.join(__dirname, '../')
-
-export default {
-  paths: {
-    root: rootPath
-  }
-}
--- mymodule/demo/App.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const App = props => (
-  <div>
-    <h1>Hello Mymodule</h1>
-  </div>
-)
-
-export default App
