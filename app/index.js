17effd398b00652e3edbd29c8be1b6edd1cdbb5d
Wallace Gleen <wallace.gleen@gmail.com>
2019-08-20 08:50:46 -0300

    refactory generator to use the available priorities methods of Generator class

--- app/index.js
+++ app/index.js
@@ -5,161 +5,178 @@ module.exports = class extends Generator {
   constructor(args, opts) {
     super(args, opts)
 
-    // eslint-disable-next-line prefer-destructuring
-    this.appName = process.argv[3]
 
-    this.log('Initializing react-create-module generator')
+
+    this.argument('appName', {
+      type: String,
+      required: true
+    })
   }
 
-  start() {
-    this.prompt([
+  initializing() {
+    const { appName } = this.options
+
+    this.log(`Initializing react-create-module generator in ${appName}`)
+  }
+
+  async prompting() {
+    this.answers = await this.prompt([
       {
         type: 'input',
-        name: 'appName',
-        message: `Enter a name for the application ${this.appName ? `or keep ${this.appName}` : ''}`
+        name: 'appDescription',
+        message: 'Enter with project description',
+        default: ''
       },
       {
         type: 'input',
-        name: 'description',
-        message: 'Enter with project description'
-      },
-      {
-        type: 'input',
-        name: 'author',
-        message: 'Enter with project author'
+        name: 'appAuthor',
+        message: 'Enter with project author',
+        default: ''
       }
     ])
-      .then((answers) => {
-        const _appName = answers.appName || this.appName
-        const appTitle = helpers.toTitle(_appName)
 
-        let destinationPath = `${_appName}/`
-        let templatePath = ''
+    const { appName } = this.options
 
-        // Root files
+    const {
+      appDescription,
+      appAuthor
+    } = this.answers
 
-        this.fs.copy(
-          this.templatePath(`${templatePath}_.gitignore`),
-          this.destinationPath(`${destinationPath}.gitignore`)
-        )
+    this.log('App name: ', appName)
+    this.log('App description: ', appDescription)
+    this.log('App author: ', appAuthor)
+  }
 
-        this.fs.copy(
-          this.templatePath(`${templatePath}.babelrc`),
-          this.destinationPath(`${destinationPath}.babelrc`)
-        )
+  writing() {
+    const { appName } = this.options
+    const appTitle = helpers.toTitle(appName)
 
-        this.fs.copy(
-          this.templatePath(`${templatePath}.editorconfig`),
-          this.destinationPath(`${destinationPath}.editorconfig`)
-        )
+    let destinationPath = `${appName}/`
+    let templatePath = ''
 
-        this.fs.copy(
-          this.templatePath(`${templatePath}.env.sample`),
-          this.destinationPath(`${destinationPath}.env.sample`)
-        )
+    // Root files
 
-        this.fs.copy(
-          this.templatePath(`${templatePath}.eslintrc.js`),
-          this.destinationPath(`${destinationPath}.eslintrc.js`)
-        )
+    this.fs.copy(
+      this.templatePath(`${templatePath}_.gitignore`),
+      this.destinationPath(`${destinationPath}.gitignore`)
+    )
 
-        this.fs.copy(
-          this.templatePath(`${templatePath}.nvmrc`),
-          this.destinationPath(`${destinationPath}.nvmrc`)
-        )
+    this.fs.copy(
+      this.templatePath(`${templatePath}.babelrc`),
+      this.destinationPath(`${destinationPath}.babelrc`)
+    )
 
-        this.fs.copy(
-          this.templatePath(`${templatePath}dotenv.js`),
-          this.destinationPath(`${destinationPath}dotenv.js`)
-        )
+    this.fs.copy(
+      this.templatePath(`${templatePath}.editorconfig`),
+      this.destinationPath(`${destinationPath}.editorconfig`)
+    )
 
-        this.fs.copyTpl(
-          this.templatePath(`${templatePath}jest.config.json.ejs`),
-          this.destinationPath(`${destinationPath}jest.config.json`),
-          { appName: _appName }
-        )
+    this.fs.copy(
+      this.templatePath(`${templatePath}.env.sample`),
+      this.destinationPath(`${destinationPath}.env.sample`)
+    )
 
-        this.fs.copyTpl(
-          this.templatePath(`${templatePath}package.json.ejs`),
-          this.destinationPath(`${destinationPath}package.json`),
-          {
-            appName: _appName,
-            description: this.description,
-            author: this.author
-          }
-        )
+    this.fs.copy(
+      this.templatePath(`${templatePath}.eslintrc.js`),
+      this.destinationPath(`${destinationPath}.eslintrc.js`)
+    )
 
-        this.fs.copy(
-          this.templatePath(`${templatePath}postcss.config.js`),
-          this.destinationPath(`${destinationPath}postcss.config.js`)
-        )
+    this.fs.copy(
+      this.templatePath(`${templatePath}.nvmrc`),
+      this.destinationPath(`${destinationPath}.nvmrc`)
+    )
 
-        this.fs.copy(
-          this.templatePath(`${templatePath}webpack.config.babel.js`),
-          this.destinationPath(`${destinationPath}webpack.config.babel.js`)
-        )
+    this.fs.copy(
+      this.templatePath(`${templatePath}dotenv.js`),
+      this.destinationPath(`${destinationPath}dotenv.js`)
+    )
 
-        // Config files
+    this.fs.copyTpl(
+      this.templatePath(`${templatePath}jest.config.json.ejs`),
+      this.destinationPath(`${destinationPath}jest.config.json`),
+      { appName }
+    )
 
-        destinationPath = `${_appName}/config/`
-        templatePath = 'config/'
+    this.fs.copyTpl(
+      this.templatePath(`${templatePath}package.json.ejs`),
+      this.destinationPath(`${destinationPath}package.json`),
+      {
+        appName,
+        description: this.description,
+        author: this.author
+      }
+    )
 
-        this.fs.copy(
-          this.templatePath(`${templatePath}webpackEnvs.js`),
-          this.destinationPath(`${destinationPath}webpackEnvs.js`)
-        )
+    this.fs.copy(
+      this.templatePath(`${templatePath}postcss.config.js`),
+      this.destinationPath(`${destinationPath}postcss.config.js`)
+    )
 
-        this.fs.copy(
-          this.templatePath(`${templatePath}index.js`),
-          this.destinationPath(`${destinationPath}index.js`)
-        )
+    this.fs.copy(
+      this.templatePath(`${templatePath}webpack.config.babel.js`),
+      this.destinationPath(`${destinationPath}webpack.config.babel.js`)
+    )
 
-        // Dev files
+    // Config files
 
-        destinationPath = `${_appName}/dev/`
-        templatePath = 'dev/'
+    destinationPath = `${appName}/config/`
+    templatePath = 'config/'
 
-        this.fs.copyTpl(
-          this.templatePath(`${templatePath}App.jsx.ejs`),
-          this.destinationPath(`${destinationPath}App.jsx`),
-          { appName: appTitle }
-        )
+    this.fs.copy(
+      this.templatePath(`${templatePath}webpackEnvs.js`),
+      this.destinationPath(`${destinationPath}webpackEnvs.js`)
+    )
 
-        this.fs.copyTpl(
-          this.templatePath(`${templatePath}index.html.ejs`),
-          this.destinationPath(`${destinationPath}index.html`),
-          { appName: appTitle }
-        )
+    this.fs.copy(
+      this.templatePath(`${templatePath}index.js`),
+      this.destinationPath(`${destinationPath}index.js`)
+    )
 
-        this.fs.copy(
-          this.templatePath(`${templatePath}index.js`),
-          this.destinationPath(`${destinationPath}index.js`)
-        )
+    // Dev files
 
-        this.fs.copy(
-          this.templatePath(`${templatePath}store.js`),
-          this.destinationPath(`${destinationPath}store.js`)
-        )
+    destinationPath = `${appName}/dev/`
+    templatePath = 'dev/'
 
-        // Src files
+    this.fs.copyTpl(
+      this.templatePath(`${templatePath}App.jsx.ejs`),
+      this.destinationPath(`${destinationPath}App.jsx`),
+      { appName: appTitle }
+    )
 
-        destinationPath = `${_appName}/src/`
-        templatePath = 'src/'
+    this.fs.copyTpl(
+      this.templatePath(`${templatePath}index.html.ejs`),
+      this.destinationPath(`${destinationPath}index.html`),
+      { appName: appTitle }
+    )
 
-        this.fs.copy(
-          this.templatePath(`${templatePath}index.js`),
-          this.destinationPath(`${destinationPath}index.js`)
-        )
+    this.fs.copy(
+      this.templatePath(`${templatePath}index.js`),
+      this.destinationPath(`${destinationPath}index.js`)
+    )
 
-        // Test
+    this.fs.copy(
+      this.templatePath(`${templatePath}store.js`),
+      this.destinationPath(`${destinationPath}store.js`)
+    )
 
-        destinationPath = `${_appName}/test/`
-        templatePath = 'test/'
+    // Src files
 
-        this.fs.copy(
-          this.templatePath(`${templatePath}`),
-          this.destinationPath(`${destinationPath}`)
-        )
-      })
+    destinationPath = `${appName}/src/`
+    templatePath = 'src/'
+
+    this.fs.copy(
+      this.templatePath(`${templatePath}index.js`),
+      this.destinationPath(`${destinationPath}index.js`)
+    )
+
+    // Test
+
+    destinationPath = `${appName}/test/`
+    templatePath = 'test/'
+
+    this.fs.copy(
+      this.templatePath(`${templatePath}`),
+      this.destinationPath(`${destinationPath}`)
+    )
   }
 }
